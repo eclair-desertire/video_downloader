5ccb25128a5a979933d4ccd1e80cfea6b456f74c
Alisher Ruziyev <eclair.desertire@gmail.com>
2022-07-05 16:14:01 +0600

    just downloading

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from cgitb import text
-from pydoc import plain
+import threading
 from tkinter import *
 from tkinter import font
 from pytube import YouTube
@@ -21,19 +20,34 @@ Label(root,text="Paste the YouTube link here:",font="arial 15 bold",
 link_enter=Entry(root, width=50, textvariable=link).place(x=32,y=90)
 
 
-def downloader():
-    YouTube(str(link.get())).streams.first().download()
+def download_video():
+    url=YouTube(str(link.get()))
+    print(url.streams)
+    video=url.streams[2]
+    video.download()
     Label(root, text="DOWNLOADED",font="arial 13",bg="#f2eee3",
     fg="red").place(x=185,y=210)
 
+def download_audio():
+    url=YouTube(str(link.get()))
+    for i in url.streams:
+        print("Formats: "+str(i))
+    
 Button(root,
-    text="DOWNLOAD",
+    text="DOWNLOAD VIDEO",
     font="arial 15 bold",
     bg="pale violet red",
     padx=2,
-    command=downloader,
+    command=download_video,
     fg="blue").place(x=180,y=150)
 
+Button(root,
+    text="DOWNLOAD ONLY AUDIO",
+    font="arial 15 bold",
+    bg="pale violet red",
+    padx=2,
+    command=download_audio,
+    fg="blue").place(x=180,y=180)
 
 
 root.mainloop()
